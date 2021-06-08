7c03536efb391c3c14d9773e675deb3f055595ea
Dlaize Kohli <dakshkohli23@gmail.com>
2021-06-08 14:16:15 +0530

    Update 1.py

--- renamer/plugins/1.py
+++ renamer/plugins/1.py
@@ -14,22 +14,22 @@ async def force_sub(c, m):
         try:
             chat = await c.get_chat_member(Config.FORCE_SUB, m.from_user.id)
             if chat.status=='kicked':
-                return await m.reply_text('Hai you are kicked from my updates channel. So, you are not able to use me',  quote=True)
+                return await m.reply_text('👻 You are kicked from my Channel, Unable to use me.',  quote=True)
 
         except UserNotParticipant:
-            button = [[InlineKeyboardButton('join Updates channel', url=f'https://t.me/{Config.FORCE_SUB}')]]
+            button = [[InlineKeyboardButton('Join Updates Channel', url=f'https://t.me/{Config.FORCE_SUB}')]]
             markup = InlineKeyboardMarkup(button)
-            return await m.reply_text(text="Hey join in my updates channel to use me.", parse_mode='markdown', reply_markup=markup, quote=True)
+            return await m.reply_text(text="Join Our Update Channel to Use Me.", parse_mode='markdown', reply_markup=markup, quote=True)
 
         except ChatAdminRequired:
-            logger.warning(f"Make me admin in @{Config.FORCE_SUB}")
+            logger.warning(f"🕴️ Make me admin in @{Config.FORCE_SUB}")
             if m.from_user.id in Config.AUTH_USERS:
-                return await m.reply_text(f"Make me admin in @{Config.FORCE_SUB}")
+                return await m.reply_text(f"🕴️ Make me admin in @{Config.FORCE_SUB}")
 
         except UsernameNotOccupied:
-            logger.warning("The forcesub username was Incorrect. Please give the correct username.")
+            logger.warning("Forcesub username was Incorrect. Please give the correct username.")
             if m.from_user.id in Config.AUTH_USERS:
-                return await m.reply_text("The forcesub username was Incorrect. Please give the correct username.")
+                return await m.reply_text("Forcesub username was Incorrect. Please give the correct username.")
 
         except Exception as e:
             if "belongs to a user" in str(e):
@@ -37,7 +37,7 @@ async def force_sub(c, m):
                 if m.from_user.id in Config.AUTH_USERS:
                     return await m.reply_text("Forcesub username must be a channel username Not yours or any other users username")
             logger.error(e)
-            return await m.reply_text("Some thing went wrong. Try again and if same issue occur contact [our group](https://t.me/Ns_Bot_supporters)", disable_web_page_preview=True, quote=True)
+            return await m.reply_text("Something went wrong. Try again and if same issue occur contact [Our Channel](https://t.me/Compass_Botz)", disable_web_page_preview=True, quote=True)
 
     await m.continue_propagation()
 
