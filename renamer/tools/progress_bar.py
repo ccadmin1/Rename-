8982b4129eb37281c8bbf9d5738dbae4bd6f695c
Dlaize Kohli <dakshkohli23@gmail.com>
2021-06-09 12:03:12 +0530

    Update progress_bar.py

--- renamer/tools/progress_bar.py
+++ renamer/tools/progress_bar.py
@@ -19,8 +19,8 @@ async def progress_bar(current, total, status_msg, start, msg):
         time_to_complete = round(((total - current) / speed)) * 1000
         time_to_complete = TimeFormatter(time_to_complete)
         progressbar = "【{0}{1}】".format(\
-            ''.join([f"▪" for i in range(math.floor(percentage / 10))]),
-            ''.join([f"▫" for i in range(10 - math.floor(percentage / 10))])
+            ''.join([f"●" for i in range(math.floor(percentage / 10))]),
+            ''.join([f"○" for i in range(10 - math.floor(percentage / 10))])
             )
         current_message = f"""**{status_msg}** - {round(percentage, 2)}%
 **┣⊸**{progressbar}
