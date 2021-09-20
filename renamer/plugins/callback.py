d5e7f76d77d39402f0b0b4e6a7b9a9f3c9fe5883
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2021-09-20 12:27:52 +0530

    Update callback.py

--- renamer/plugins/callback.py
+++ renamer/plugins/callback.py
@@ -1,1 +1,62 @@
 
+import logging
+logger = logging.getLogger(__name__)
+
+from .commands import *
+from ..config import Config
+from ..tools.text import TEXT
+from pyrogram import Client as Compass_Botz, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserBannedInChannel, UserNotParticipant
+from pyrogram.emoji import *
+
+
+################## Callback for help button ##################
+
+@Compass_Botz.on_callback_query(filters.regex('^help$'))
+async def help_cb(c, m):
+    await m.answer()
+    await help(c, m, True)
+
+
+################## Callback for donate button ##################
+
+@Compass_Botz.on_callback_query(filters.regex('^donate$'))
+async def donate(c, m):
+    button = [[
+        InlineKeyboardButton(f'⚡ Home', callback_data='back'),
+        InlineKeyboardButton(f'❗ About', callback_data='about')
+        ],[
+        InlineKeyboardButton(f'✖️ Close', callback_data='close')
+    ]]
+    reply_markup = InlineKeyboardMarkup(button)
+    await m.answer()
+    await m.message.edit(
+        text=TEXT.DONATE_USER.format(m.from_user.first_name),
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
+
+################## Callback for close button ##################
+
+@Compass_Botz.on_callback_query(filters.regex('^close$'))
+async def close_cb(c, m):
+    await m.message.delete()
+    await m.message.reply_to_message.delete()
+
+
+################## Callback for home button ##################
+
+@Compass_Botz.on_callback_query(filters.regex('^back$'))
+async def back_cb(c, m):
+    await m.answer()
+    await start(c, m, True)
+
+
+################## Callback for about button ##################
+
+@Compass_Botz.on_callback_query(filters.regex('^about$'))
+async def about_cb(c, m):
+    await m.answer()
+    await about(c, m, True)
